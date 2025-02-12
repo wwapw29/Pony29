b8b52d5320ef3bd3a5ee3f0859415630621f6b4a
wwapw29 <274120497@qq.com>
2025-02-13 02:52:24 +0800

    添加项目文件。

---
+++ CS/Chat.cs
@@ -0,0 +1,21 @@
+﻿namespace Pony29.CS
+{
+    using Markdig;
+    using OpenAI;
+    public class Chat
+    {
+
+
+    }
+
+    
+
+public class MarkdownService
+    {
+        public string ConvertToHtml(string markdown)
+        {
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            return Markdown.ToHtml(markdown, pipeline);
+        }
+    }
+}
